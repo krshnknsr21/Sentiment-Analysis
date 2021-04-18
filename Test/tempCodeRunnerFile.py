bbec44476000f1d149c9d9ed16088724ca0f4152
krshnknsr21 <krshnknsr21@github.com>
2021-04-18 22:26:32 +0530

    Added Labels to the screen, updated the ppt

--- Test/tempCodeRunnerFile.py
+++ Test/tempCodeRunnerFile.py
@@ -1,12 +1,1 @@
-from kivy.app import App
-from kivy.uix.dropdown import DropDown
-from kivy.uix.boxlayout import BoxLayout
-
-class CustomDropDown(BoxLayout):
-    pass
-
-class MainApp(App):
-    def build(self):
-        return CustomDropDown()
-if __name__=='__main__':
-    MainApp().run()
+base = [0,0]#, 0, 0]
--- Test/test.py
+++ Test/test.py
@@ -1,12 +1,38 @@
-from kivy.app import App
-from kivy.uix.dropdown import DropDown
-from kivy.uix.boxlayout import BoxLayout
+n = int(input())
+dir1 = input().split()
+dir2 = input().split()
+dir3 = input().split()
+dir4 = input().split()
+waves = [dir1, dir2, dir3, dir4]
+sumlist = [0, 0, 0, 0]
 
-class CustomDropDown(BoxLayout):
-    pass
+def popitem():
+    for i in waves:
+        i.pop(0)
+    
+def do_sum(x, ind):
+    for i in x:
+        sumlist[ind]+=int(i)
+     
+for i in range(0, 4):
+        do_sum(waves[i], i)
 
-class MainApp(App):
-    def build(self):
-        return CustomDropDown()
-if __name__=='__main__':
-    MainApp().run()
+def check_same():
+    c = 0
+    for i in range(1,4):
+        if sumlist[0] == sumlist[i]:
+            c += 1
+    if c == 3:
+        return True
+    else:
+        return False
+
+for v in range(0, n):
+    if check_same():
+        print(sumlist[0])
+        exit()
+    else:
+        for i in range(0,4):
+            sumlist[i] -= int(waves[i][0]) 
+        popitem()
+        
